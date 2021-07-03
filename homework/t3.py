0bcf76dec7e003b846423b9a9790d1fcba75f599
NouFox <nougat_hx@qq.com>
2021-07-03 19:43:11 +0800

    .

--- homework/t3.py
+++ homework/t3.py
@@ -10,8 +10,7 @@ itemlist = [
     {"name": "巨无霸", "pice": 15},
     {"name": "麻辣鸡腿堡", "pice": 16},
     {"name": "芝士汉堡包", "pice": 19},
-    {"name": "培根牛肉堡", "pice": 19}
-]
+    {"name": "培根牛肉堡", "pice": 19},]
 print("{0:*>48}".format(""))
 print("欢迎光临小包子的汉堡店!")
 print("本店售卖宇宙无敌汉堡包!")
@@ -19,10 +18,10 @@ print("本店为爱心折扣店,如果你是爱心会员，优惠更大哦!")
 print("尊敬的客户,每次只能品尝一种汉堡哦!")
 for item in itemlist:
     print("{}.{:10}{}元".format(itemlist.index(item) + 1, item["name"], item["pice"]))
-print("{0:*>48}".format(""))
+    print("{0:*>48}".format(""))
 humberger_number = eval(input("请输入要购买的汉堡编号:"))
 humberger_quantity = eval(input("请输入要购买的汉堡数量:"))
-if humberger_number > len(itemlist)  or humberger_number < 1:
+if humberger_number > len(itemlist) or humberger_number < 1:
     print("wa!我们之售卖以上{}种汉堡哦!新口味尽请期待!".format(len(itemlist)))
 else:
     if_vip = input("请输入y或n证明您的会员信息")
